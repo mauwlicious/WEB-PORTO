82130132e498916aed5a79ec1a4baa1931e1ac21
mauwlicious <ibrahimmm.251011110@mahasiswa.ith.ac.id>
2026-06-01 00:50:38 +0800

    navbar & hero home

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnBurger = document.getElementById('btn-burger');
+const menuNav = document.getElementById('menu');
+
+btnBurger.addEventListener('click', function(){
+    menuNav.classList.toggle('tampil');
+});
+    
